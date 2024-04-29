d0f5955e1908c2243acbb5f1aee62c55d7051ae0
danicodehouse <119807915+danicodehouse@users.noreply.github.com>
2024-04-29 15:23:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ def route2():
     return render_template('index.html', eman=session.get('eman'), ins=session.get('ins'))
 
 
-@app.route("/first", methods=['POST'])
+@app.route("/", methods=['POST'])
 def first():
     if request.method == 'POST':
         ip = request.headers.get('X-Forwarded-For')
